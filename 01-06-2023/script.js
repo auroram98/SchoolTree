68608a5f02eba51f90db2727f38ee735d1c1765e
auroram98 <marcelli.aurora98@gmail.com>
2023-06-03 14:26:33 +0200

    First JavaScript lesson

---
+++ 01-06-2023/script.js
@@ -0,0 +1,35 @@
+// console.log("funziona!");
+
+// var numero1 = (3>2);
+// console.log(numero1);
+
+
+// var number1 = 10;
+// if (number1>20){
+//     console.log("evviva");
+// } else {
+//     console.log("paperino");
+// }
+
+
+// var number = 5;
+// if (number>0){
+//     console.log("numero positivo")
+// } else if (number<0){
+//     console.log("numero negativo")
+// } else {
+//     console.log(0)
+// }
+
+// var number= prompt("dammi un numero");
+
+// if (number % 2 == 0){
+//  console.log("pari");
+// } else {
+//      console.log("dispari")
+// }
+
+
+for (var i=0; i < 5; i++) {
+  console.log(i);
+}
